50bccfb0239c82b49676f6fde12e945f26558032
Maksim <Lukianchukma@gmail.com>
2020-04-12 22:23:45 +0300

    refactor: Отрефачил стуркутру файлов

--- src/panels/Home/Home.js
+++ src/panels/Home/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
@@ -12,7 +11,7 @@ const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Example</PanelHeader>
 		{fetchedUser &&
-		<Group title="User Data Fetched with VK Bridge">
+		<Group title='User Data Fetched with VK Bridge'>
 			<Cell
 				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
 				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
@@ -21,27 +20,14 @@ const Home = ({ id, go, fetchedUser }) => (
 			</Cell>
 		</Group>}
 
-		<Group title="Navigation Example">
+		<Group title='Navigation Example'>
 			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
+				<Button size='xl' level='2' onClick={go} data-to='persik'>
 					Show me the Persik, please
 				</Button>
 			</Div>
 		</Group>
 	</Panel>
 );
-
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
 
 export default Home;
